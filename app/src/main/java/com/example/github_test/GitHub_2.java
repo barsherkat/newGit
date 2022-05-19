899ca6264e9e26618b4695e3d862bc5a1391d975
android <barsherkat@gmail.com>
2022-05-19 09:57:50 +0430

    new activity

---
+++ app/src/main/java/com/example/github_test/GitHub_2.java
@@ -0,0 +1,14 @@
+package com.example.github_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GitHub_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git_hub_2);
+    }
+}
