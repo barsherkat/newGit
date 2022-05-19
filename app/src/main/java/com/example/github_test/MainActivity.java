f69b6eb9209872092f082e7951bf958faa2e69d8
android <barsherkat@gmail.com>
2022-05-19 09:17:31 +0430

    add update

--- app/src/main/java/com/example/github_test/MainActivity.java
+++ app/src/main/java/com/example/github_test/MainActivity.java
@@ -10,5 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //example
+        a();
+
     }
+
+    public void a()
+    {
+        int c=3+7;
+    }
+
+
+
 }
